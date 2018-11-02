fad9dae942e0b362f59ea6dfb53a6d98bccf8d7f
Blagica Stojanovska <blagi.stojanovska@hotmail.com>
2018-11-02 13:16:26 +0100

    Updates

---
+++ precache-manifest.e50323fba5b9d806a0bf007594f0a839.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "09871e20c30ff3ce6b175593a6f714b6",
+    "url": "/gaming/static/media/03.09871e20.png"
+  },
+  {
+    "revision": "61c1a670fe9247b7b206",
+    "url": "/gaming/static/css/main.fc735684.chunk.css"
+  },
+  {
+    "revision": "eebf2f8e6cb4730de541",
+    "url": "/gaming/static/js/1.eebf2f8e.chunk.js"
+  },
+  {
+    "revision": "c9c3d0efef894c8bb6b7",
+    "url": "/gaming/static/js/runtime~main.c9c3d0ef.js"
+  },
+  {
+    "revision": "b48fe09b148f195f47439945f9d2cd35",
+    "url": "/gaming/static/media/01.b48fe09b.png"
+  },
+  {
+    "revision": "aa1ba4eb7bf9ed84d97317d0264f1bc8",
+    "url": "/gaming/static/media/02.aa1ba4eb.png"
+  },
+  {
+    "revision": "61c1a670fe9247b7b206",
+    "url": "/gaming/static/js/main.61c1a670.chunk.js"
+  },
+  {
+    "revision": "0b078d6550da23b92e4befd442f8eca9",
+    "url": "/gaming/static/media/04.0b078d65.png"
+  },
+  {
+    "revision": "59de13bfc0f2bd3b00529610c0f56801",
+    "url": "/gaming/static/media/05.59de13bf.png"
+  },
+  {
+    "revision": "6a9fb2ed267819dd5076b8865f849eeb",
+    "url": "/gaming/static/media/instagram.6a9fb2ed.png"
+  },
+  {
+    "revision": "9a513524f2be4401637c4fee6191cd2e",
+    "url": "/gaming/static/media/twitter.9a513524.png"
+  },
+  {
+    "revision": "d3bb3fbdccf57f1fa39993b6af0e5fe5",
+    "url": "/gaming/index.html"
+  }
+];
