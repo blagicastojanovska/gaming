5a437aa7e372b9a1955eaa1c8a51b836e08e8afd
Blagica Stojanovska <blagica@steets.nl>
2019-12-16 15:16:55 +0100

    updated the Navbar with hooks & made the App a function component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import './styles/App.sass';
@@ -10,86 +10,61 @@ import Gamepage from './components/Gamepage.js';
 import Scrolltotop from './components/Scrolltotop.js';
 import ScrollUp from 'react-scroll-up';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      position: ''
-    };
-  }
-
-  toggleFixed = () => {
-    this.setState({
-      position: 'fixed'
-    });
+const App = () => {
+  const scrollButton = {
+    position: 'fixed',
+    bottom: 100,
+    right: 20,
+    cursor: 'pointer',
+    transitionDuration: '0.2s',
+    transitionTimingFunction: 'linear',
+    transitionDelay: '0s'
   };
 
-  clearFixed = () => {
-    this.setState({
-      position: ''
-    });
-  };
-
-  render() {
-    const style = {
-      position: this.state.position
-    };
-
-    const scrollButton = {
-      position: 'fixed',
-      bottom: 100,
-      right: 20,
-      cursor: 'pointer',
-      transitionDuration: '0.2s',
-      transitionTimingFunction: 'linear',
-      transitionDelay: '0s'
-    };
-
-    return (
-      <Router>
-        <Scrolltotop>
-          <div className="App" style={style}>
-            <Helmet>
-              <title>BrainsterBox</title>
-              <meta property="og:title" content="BrainsterBox" />
-              <meta
-                name="og:description"
-                content="Вашата лична кутија со ресурси и алатки за креативна колаборација и
+  return (
+    <Router>
+      <Scrolltotop>
+        <div className="App">
+          <Helmet>
+            <title>BrainsterBox</title>
+            <meta property="og:title" content="BrainsterBox" />
+            <meta
+              name="og:description"
+              content="Вашата лична кутија со ресурси и алатки за креативна колаборација и
                  откривање на потенцијалот во твојот тим или организација."
-              />
-              <meta
-                property="og:image"
-                content="https://toolbox.hyperisland.com/images/fb-og.png"
-              />
-              <meta property="og:type" content="article" />
-              <meta name="author" content="Blagica Stojanovska" />
-              <meta
-                name="og:url"
-                content="https://blagicastojanovska.github.io/gaming/"
-              />
-              <meta
-                name="base_url"
-                content="https://blagicastojanovska.github.io/gaming/"
-              />
-            </Helmet>
-            <Navbar toggleFixed={this.toggleFixed} clearFixed={this.clearFixed} />
-            <Switch>
-              <Route exact path="/" component={Gamecards} />
-              <Route path="/Gamepage/:id" component={Gamepage} />
-            </Switch>
-            <Staffpicks />
-            <Footer />
-            <ScrollUp showUnder={160} style={scrollButton}>
-              <div className="text-center buttonUp">
-                <i className="fas fa-angle-up fa-3x"></i>
-                <span>ПОЧЕТОК</span>
-              </div>
-            </ScrollUp>
-          </div>
-        </Scrolltotop>
-      </Router>
-    );
-  }
-}
+            />
+            <meta
+              property="og:image"
+              content="https://toolbox.hyperisland.com/images/fb-og.png"
+            />
+            <meta property="og:type" content="article" />
+            <meta name="author" content="Blagica Stojanovska" />
+            <meta
+              name="og:url"
+              content="https://blagicastojanovska.github.io/gaming/"
+            />
+            <meta
+              name="base_url"
+              content="https://blagicastojanovska.github.io/gaming/"
+            />
+          </Helmet>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Gamecards} />
+            <Route path="/Gamepage/:id" component={Gamepage} />
+          </Switch>
+          <Staffpicks />
+          <Footer />
+          <ScrollUp showUnder={160} style={scrollButton}>
+            <div className="text-center buttonUp">
+              <i className="fas fa-angle-up fa-3x"></i>
+              <span>ПОЧЕТОК</span>
+            </div>
+          </ScrollUp>
+        </div>
+      </Scrolltotop>
+    </Router>
+  );
+};
 
 export default App;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,79 +1,70 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Header.js';
 import NavbarMenu from './NavbarMenu.js';
 import { Link } from 'react-router-dom';
 
-class Navbar extends Component {
-	constructor() {
-		super();
-		this.state = {
-			active: true,
-			menu: false
-		};
-	}
+const Navbar = () => {
+	const [navbarInfo, setNavbarInfo] = useState({
+		active: true,
+		menu: false
+	});
 
-	toggleHeader = () => {
-		this.setState({
-			active: !this.state.active,
-			menu: false
-		});
-		this.props.clearFixed();
+	useEffect(() => {
+		navbarInfo.menu &&
+			document.body.setAttribute(
+				'style',
+				'position: fixed; width: 100%; height: 100%; overflow: hidden'
+			);
+
+		return () => document.body.removeAttribute('style');
+	}, [navbarInfo.menu]);
+
+	const toggleHeader = () => {
+		setNavbarInfo({ active: !navbarInfo.active, menu: false });
 	};
 
-	showMenu = () => {
-		this.props.toggleFixed();
-		this.setState({
-			menu: !this.state.menu,
-			active: false
-		});
+	const showMenu = () => {
+		setNavbarInfo({ menu: !navbarInfo.menu, active: false });
 	};
 
-	close = () => {
-		this.props.clearFixed();
-		this.setState({
-			active: true,
-			menu: false
-		});
+	const close = () => {
+		setNavbarInfo({ active: false, menu: false });
 	};
 
-	render() {
-		const { menu, active } = this.state;
-
-		return (
-			<div className="Navbar">
-				<div className="container">
-					<div className="row navbar-flex">
-						<div className="col-md-2 col-sm-3 col-xs-5">
-							<Link to="/">
-								<img
-									src={require('../assets/img/Logo.png')}
-									alt="Logo-img"
-									className="img img-responsive"
-								/>
-							</Link>
-						</div>
-						<div className="col-md-2 col-md-offset-8 col-sm-2 col-sm-offset-7 col-xs-2 col-xs-offset-5">
-							<span className="navbar-question" onClick={this.toggleHeader}>
-								?
-							</span>
-							<h4 className="navbar-h4 hidden-xs">МЕНИ</h4>
-							{menu ? (
-								<span className="closed" onClick={this.close}>
-									✕
-								</span>
-							) : (
-								<span className="hamburger" onClick={this.showMenu}>
-									☰
-								</span>
-							)}
-						</div>
+	return (
+		<div className="Navbar">
+			<div className="container">
+				<div className="row navbar-flex">
+					<div className="col-md-2 col-sm-3 col-xs-5">
+						<Link to="/">
+							<img
+								src={require('../assets/img/Logo.png')}
+								alt="Logo-img"
+								className="img img-responsive"
+							/>
+						</Link>
 					</div>
-					<div className="row">{menu && <NavbarMenu />}</div>
+					<div className="col-md-2 col-md-offset-8 col-sm-2 col-sm-offset-7 col-xs-2 col-xs-offset-5">
+						<span className="navbar-question" onClick={toggleHeader}>
+							?
+						</span>
+						<h4 className="navbar-h4 hidden-xs">МЕНИ</h4>
+						{navbarInfo.menu ? (
+							<span className="closed" onClick={close}>
+								✕
+							</span>
+						) : (
+							<span className="hamburger" onClick={showMenu}>
+								☰
+							</span>
+						)}
+					</div>
 				</div>
-				{active && <Header toggleHeader={this.toggleHeader} />}
+				<div className="row">{navbarInfo.menu && <NavbarMenu />}</div>
 			</div>
-		);
-	}
-}
+			{navbarInfo.active && <Header toggleHeader={toggleHeader} />}
+		</div>
+	);
+};
 
 export default Navbar;
