f546a20a9dedccc8bb12286f93ad50bc87f147e6
Blagica Stojanovska <blagica@steets.nl>
2019-12-16 16:44:28 +0100

    updated staffpicks with hooks

--- src/Components/Staffpick.js
+++ src/Components/Staffpick.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Staffpick = ({ style, category, title, description }) => {
+const Staffpick = ({ bgImg, category, title, description }) => {
   return (
     <div className="col-md-3 col-sm-6 col-xs-6">
       <div className="Staffpick">
-        <div className="staff-image-div" style={style}></div>
+        <div className="staff-image-div" style={bgImg}></div>
         <h4>
           <span>|||</span>
           {category}
--- src/Components/Staffpicks.js
+++ src/Components/Staffpicks.js
@@ -1,67 +1,57 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Staffpick from './Staffpick.js';
 import { Link } from 'react-router-dom';
 
-class Staffpicks extends Component {
-	constructor() {
-		super();
-		this.state = {
-			games: []
-		};
-	}
+const Staffpicks = props => {
+	const [games, setGames] = useState([]);
 
-	async componentDidMount() {
-		var games = await axios.get('https://project3-server.herokuapp.com/posts');
-		this.setState({
-			games: games.data.filter(
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	const fetchData = async () => {
+		const result = await axios('https://project3-server.herokuapp.com/posts');
+
+		setGames(
+			result.data.filter(
 				game =>
-					games.data[2].id === game.id ||
-					games.data[13].id === game.id ||
-					games.data[20].id === game.id ||
-					games.data[41].id === game.id
+					result.data[2].id === game.id ||
+					result.data[13].id === game.id ||
+					result.data[20].id === game.id ||
+					result.data[41].id === game.id
 			)
-		});
-	}
+		);
+	};
 
-	render() {
-		const { games } = this.state;
-		
-		return (
-			<div className="Staffpicks" id="staffpicks">
-				<div className="container">
-					<div className="row">
-						<div className="col-md-12">
-							<h4>Препорачани од нашиот тим</h4>
-							<hr className="smallerHr" />
-							<hr />
-						</div>
-					</div>
-					<div className="row">
-						{games.map(game => {
-							const style = {
-								backgroundImage: `url(${require(`../assets/img/img-cards/${game.image}.png`)})`,
-								backgroundPosition: 'center',
-								backgroundSize: 'contain',
-								backgroundRepeat: 'no-repeat'
-							};
-							return (
-								<Link to={`/Gamepage/${game.id}`} key={game.id}>
-									<Staffpick
-										key={game.id}
-										title={game.title}
-										category={game.category}
-										style={style}
-										description={game.description}
-									/>
-								</Link>
-							);
-						})}
+	return (
+		<div className="Staffpicks" id="staffpicks">
+			<div className="container">
+				<div className="row">
+					<div className="col-md-12">
+						<h4>Препорачани од нашиот тим</h4>
+						<hr className="smallerHr" />
+						<hr />
 					</div>
 				</div>
+				<div className="row">
+					{games.map(game => (
+						<Link to={`/Gamepage/${game.id}`} key={game.id}>
+							<Staffpick
+								key={game.id}
+								title={game.title}
+								category={game.category}
+								bgImg={{
+									backgroundImage: `url(${require(`../assets/img/img-cards/${game.image}.png`)})`
+								}}
+								description={game.description}
+							/>
+						</Link>
+					))}
+				</div>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
 export default Staffpicks;
